d5aba1b7fca4d4b8965108a1fad8c7440c723091
Ali <139615077+MOTOMADEN@users.noreply.github.com>
2026-08-03 13:54:48 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/taskmanager/',  
+  base: '/taskManagement/',  
   css: {
     modules: {
       localsConvention: 'camelCase'
